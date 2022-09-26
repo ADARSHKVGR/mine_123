11537795a2aec4f8206a356a09ae197a6cda3f68
adarsh <kvadarsh91@gmai.com>
2022-09-26 22:35:51 +0530

    first commit

---
+++ bin/mine.dart
@@ -0,0 +1,5 @@
+import 'package:mine/mine.dart' as mine;
+
+void main(List<String> arguments) {
+  print('Hello world: ${mine.calculate()}!');
+}
---
+++ test/mine_test.dart
@@ -0,0 +1,8 @@
+import 'package:mine/mine.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
